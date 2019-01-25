11ccea07fdb968ba34c53b71e0ddb84e59d57781
Matthew Kelly <mkonabike@gmail.com>
2019-01-25 10:51:38 -0600

    added Person class with name and age with methods

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 // CLASSES USING PROTOTYPE
-function User(name) {
+function User(name,age) {
 	this.name = name;
+	this.age = age;
 }
 
 User.prototype.sayHi = function() {
-	alert(this.name);
+	console.log(this.name);
 }
 
-let user = new User("John");
+User.prototype.getAge = function () {
+	console.log(this.age);
+}
+
+
+let user = new User("John",58);
 user.sayHi();
+user.getAge();
 
 // USING CLASS SYNTAX
+class Person {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+
+	getName() {
+		console.log(this.name)
+	}
+
+	getAge() {
+		console.log(this.age)
+	}
+
+	sayHi() {
+		console.log(this.name)
+	}
+}
+
+let Matt = new Person("Matt Kelly", 38)
+Matt.sayHi()
+
+Matt.getAge()
